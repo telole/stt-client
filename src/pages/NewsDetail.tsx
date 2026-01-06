644c70cc2ca1f057930a1af5d524a0c2c60fe69f
telole <asololearya@gmail.com>
2026-01-06 16:00:53 +0700

    feat:newsDetail

---
+++ src/pages/NewsDetail.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+
+const NewsDetail = () => {
+  return (
+    <div className="flex w-full max-w-[1920px] pt-[90px] flex-col gap-2.5 items-start flex-nowrap relative mx-auto">
+      <div className="flex w-full max-w-[1920px] flex-col items-start flex-shrink-0 flex-nowrap relative">
+        <div className="flex w-full max-w-[1920px] px-[410px] py-[68px] flex-col gap-[30px] items-center flex-shrink-0 flex-nowrap bg-white relative z-[1]">
+          <div className="flex min-w-0 flex-col gap-1.5 items-start self-stretch flex-shrink-0 flex-nowrap relative z-[2]">
+            <div className="flex w-[87px] items-center flex-shrink-0 flex-nowrap relative z-[3] cursor-pointer">
+              <div
+                className="w-5 h-5 flex-shrink-0 bg-center bg-cover bg-no-repeat relative overflow-hidden z-[4]"
+                style={{
+                  backgroundImage: 'url(https://codia-f2c.s3.us-west-1.amazonaws.com/image/2026-01-06/DOkGi5sgqS.png)',
+                }}
+              ></div>
+              <span className="h-5 flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-normal leading-[19.504px] text-black/50 relative text-left whitespace-nowrap z-[5]">
+                Kembali
+              </span>
+            </div>
+            
+            <span className="h-[29px] self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-2xl font-bold leading-[29px] text-black relative text-left whitespace-nowrap z-[6]">
+              Kreativitas Tanpa Batas: Menuju Indonesia Emas di Era Industri 4.0
+            </span>
+            
+            <span className="h-5 self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-normal leading-[19.504px] text-[#206fa0] relative text-left whitespace-nowrap z-[7]">
+              Pati, 3 Oktober 2024
+            </span>
+          </div>
+          
+          <div
+            className="w-[550px] h-[367px] flex-shrink-0 bg-center bg-cover bg-no-repeat rounded-[10px] relative z-[8]"
+            style={{
+              backgroundImage: 'url(https://codia-f2c.s3.us-west-1.amazonaws.com/image/2026-01-06/bvQcSzUcDU.png)',
+            }}
+          ></div>
+          
+          <span className="flex w-[1100px] min-w-0 h-20 items-start self-stretch flex-shrink-0 font-['Montserrat'] text-base font-normal leading-[19.504px] text-black relative text-justify z-[9]">
+            Era Industri 4.0 telah membawa perubahan besar dalam berbagai aspek
+            kehidupan manusia. Perkembangan teknologi digital seperti kecerdasan
+            buatan (Artificial Intelligence), Internet of Things (IoT), big
+            data, dan otomatisasi telah mengubah cara manusia bekerja, belajar,
+            dan berinovasi. Di tengah arus perubahan ini, kreativitas menjadi
+            kunci utama dalam mendorong kemajuan bangsa menuju visi Indonesia
+            Emas.
+          </span>
+          
+          <span className="min-w-0 h-6 self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-xl font-bold leading-6 text-black relative text-justify whitespace-nowrap z-[10]">
+            Peran Kreativitas di Era Industri 4.0
+          </span>
+          
+          <span className="flex w-[1100px] min-w-0 h-20 items-start self-stretch flex-shrink-0 font-['Montserrat'] text-base font-normal leading-[19.504px] text-black relative text-justify z-[11]">
+            Kreativitas tidak lagi terbatas pada bidang seni semata, tetapi
+            telah menjadi fondasi utama dalam dunia industri, pendidikan, dan
+            teknologi. Di era Industri 4.0, kreativitas berperan sebagai
+            kemampuan untuk melihat peluang, memecahkan masalah secara inovatif,
+            serta menciptakan solusi yang relevan dengan kebutuhan zaman.
+            Individu yang kreatif mampu beradaptasi dengan cepat terhadap
+            perubahan dan menciptakan nilai baru dari kemajuan teknologi.
+          </span>
+        </div>
+        
+        <div className="w-full max-w-[1920px] h-[492px] flex-shrink-0 relative z-[12]">
+          <div className="w-full max-w-[1920px] h-[492px] bg-[#052437] absolute top-0 left-1/2 transform -translate-x-1/2 z-[13]"></div>
+          
+          <div className="flex w-[1087px] gap-[50px] justify-center items-start flex-wrap relative z-[14] mt-[140px] ml-[416.5px]">
+            <div className="flex w-[446px] flex-col gap-[15px] items-start flex-nowrap relative z-[15]">
+              <span className="min-w-0 h-5 self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-bold leading-[19.504px] text-white relative text-left whitespace-nowrap z-[16]">
+                Raih Mimpimu Bersama Kami
+              </span>
+              
+              <span className="flex w-[379px] h-[60px] items-start flex-shrink-0 font-['Montserrat'] text-base font-normal leading-[19.504px] text-white relative text-justify z-[17]">
+                Sekolahmu adalah langkah awal menuju cita-citamu. Segera
+                daftarkan dirimu, kami siap melayani!
+              </span>
+              
+              <div className="flex min-w-0 gap-[30px] items-center self-stretch flex-shrink-0 flex-nowrap relative z-[18]">
+                <div className="flex w-[177px] px-5 py-[15px] gap-2.5 justify-center items-center flex-shrink-0 flex-nowrap bg-[#f0cd02] rounded-[10px] relative z-[19] cursor-pointer hover:bg-[#d9b802] transition-colors">
+                  <span className="h-5 flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-bold leading-[19.504px] text-[#0b466a] relative text-left whitespace-nowrap z-[20]">
+                    Daftar Sekarang
+                  </span>
+                </div>
+                
+                <div className="flex w-[239px] px-5 py-[15px] gap-2.5 justify-center items-center flex-shrink-0 flex-nowrap bg-[#0b466a] rounded-[10px] relative z-[21] cursor-pointer hover:bg-[#093a56] transition-colors">
+                  <span className="h-5 flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-bold leading-[19.504px] text-white relative text-left whitespace-nowrap z-[22]">
+                    Hubungi Via WhatsApp
+                  </span>
+                </div>
+              </div>
+            </div>
+            
+            <div className="flex w-[436px] flex-col gap-[15px] items-start flex-nowrap relative z-[23]">
+              <span className="min-w-0 h-5 self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-bold leading-[19.504px] text-white relative text-left whitespace-nowrap z-[24]">
+                Hubungi Kami
+              </span>
+              
+              <span className="flex w-[436px] min-w-0 h-20 items-start self-stretch flex-shrink-0 font-['Montserrat'] text-base font-normal leading-[19.504px] text-white relative text-justify z-[25]">
+                Ngepungrojo, Kec. Pati, Kabupaten Pati, Jawa Tengah 59119<br /><br />(0295)
+                382470
+              </span>
+            </div>
+            
+            <div className="flex w-[105px] flex-col gap-[15px] items-start flex-nowrap relative z-[26]">
+              <span className="h-5 self-stretch flex-shrink-0 flex-basis-auto font-['Montserrat'] text-base font-bold leading-[19.504px] text-white relative text-left whitespace-nowrap z-[27]">
+                Sosial Media
+              </span>
+              
+              <span className="flex w-[105px] h-24 items-start self-stretch flex-shrink-0 font-['Montserrat'] text-base font-normal leading-6 text-white relative text-justify overflow-hidden text-overflow-initial z-[28]">
+                Facebook <br />Instagram <br />TikTok <br />YouTube
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsDetail;
